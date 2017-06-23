78bab9b16e7c8b3c0f030a382965c427f30c1ba2
Per Salomonsen <psalomonsen@belega.dk>
2017-06-23 13:09:14 +0200

    Stream of array sub-sequence
    Created overloaded version of Streams::of, which can take an array, a
    starting offset and length of the sequence to create a stream.

--- src/main/java/dk/belega/util/stream/Streams.java
+++ src/main/java/dk/belega/util/stream/Streams.java
@@ -11,24 +11,37 @@ public class Streams {
 
     /**
      * Return a stream containing the given elements, in order.
+     *
      * @param elements the elements for the stream
-     * @param <T> the stream value type
+     * @param <T>      the stream value type
      * @return a stream
      */
     @SafeVarargs
     public static <T> Stream<T> of(T... elements) {
-        return arrayFromOffset(0, elements);
+        return of(elements, 0, elements.length);
     }
 
-    //////////////////////////////////////////////////////////////////////////////////////////////
-    // Implementation
+    /**
+     * Return a stream containing {@code length} elements from the array, starting at the given
+     * {@code offset}.
+     *
+     * @param elements the array containing elements
+     * @param offset   the starting offset
+     * @param length   the length of the stream
+     * @param <T>      the stream value type
+     * @return stream of elements from the array
+     */
+    public static <T> Stream<T> of(T[] elements, int offset, int length) {
 
-    @SafeVarargs
-    private static <T> Stream<T> arrayFromOffset(int offset, T... elements) {
-        if (offset < elements.length) {
-            return Stream.cons(() -> elements[offset], () -> arrayFromOffset(offset + 1, elements));
-        } else {
+        if (length < 0)
+            throw new IllegalArgumentException("length cannot be negative");
+
+        if (0 == length) {
             return Stream.nil();
+        } else {
+            return Stream.cons(
+                    () -> elements[offset],
+                    () -> of(elements, offset + 1, length - 1));
         }
     }
 }
--- src/test/java/dk/belega/util/stream/StreamsTest.java
+++ src/test/java/dk/belega/util/stream/StreamsTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import static org.junit.Assert.fail;
+
 /**
  * Unit tests for the {@link Streams} class.
  */
@@ -27,5 +29,37 @@ public class StreamsTest extends AbstractTest {
 
         // Then the result is a stream returning the values in order
         assertStreamEquals(EXPECTED_RESULT, actualResult);
+    }
+
+    @Test
+    public void testOfNegativeLength() {
+
+        try {
+            // When creating a stream with negative length
+            Streams.of(new String[0], 0, -1);
+
+            // Then the call fails
+            fail("expected Streams::of to fail for negative length");
+
+        } catch (IllegalArgumentException ignored) {
+            // And throws an exception
+        }
+    }
+
+    @Test
+    public void testOfOutOfBounds() {
+
+        // Given a stream created with invalid offset
+        final Stream<String> stream = Streams.of(new String[1], 1, 1);
+
+        try {
+            // When accessing an invalid element
+            stream.getHead();
+
+            // Then the call fails
+            fail("expected Stream::getHead to fail on invalid element");
+        } catch (ArrayIndexOutOfBoundsException ignored) {
+            // And throws an exception
+        }
     }
 }
